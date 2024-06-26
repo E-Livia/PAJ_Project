a72259f6af6f2635ad9f2c36d0181abf57285106
E-Livia <livia.popa.3@gmail.com>
2024-06-26 14:43:44 +0300

    added add event with file explorer on date

--- calendar_frontend/src/app/features/main-page/main-page/main-page.component.ts
+++ calendar_frontend/src/app/features/main-page/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NzCalendarMode } from 'ng-zorro-antd/calendar';
 
 @Component({
@@ -7,14 +7,28 @@ import { NzCalendarMode } from 'ng-zorro-antd/calendar';
   styleUrl: './main-page.component.scss',
 })
 export class MainPageComponent {
+  @ViewChild('fileInput') fileInput!: ElementRef;
   selectedDate: Date = new Date(); // Data selectată inițial
   events = []; // Array-ul de evenimente
+  showAddEventTab: boolean = false;
 
   constructor() {
   }
 
   onDateChange(date: Date): void {
     this.selectedDate = date;
+    this.showAddEventTab = true;
   }
 
+  triggerFileInput() {
+    this.fileInput.nativeElement.click(); // Deschide File Explorer
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      console.log('Fișier selectat:', file);
+      // Prelucrarea fișierului aici, de exemplu încărcare
+    }
+  }
 }
